39195ea6d632a9174f0e9c1f1bea19b402d05672
hzlshen <m15277968105@163.com>
2018-03-18 14:05:54 +0800

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,34 @@
+import {Component, ViewChild} from '@angular/core';
+import {NavController, Slide, Slides} from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  @ViewChild(Slides) slides:Slides;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  goToSlide(){
+    this.slides.slideTo(2,500);
+  }
+
+  slideChanged(){
+    let currentIndex = this.slides.getActiveIndex();
+    console.log('index is',currentIndex);
+  }
+
+  ngAfterViewInit() {
+    this.slides.freeMode = true;
+  }
+
+  //初始化slides
+  initSlides(){
+
+  }
+
+}
