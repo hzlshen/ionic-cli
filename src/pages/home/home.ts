f860861a50f8f56a2db4ed6c30c7d9e969ec704f
hzlshen <m15277968105@163.com>
2018-03-27 11:13:25 +0800

    search oj8k

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,10 +9,15 @@ export class HomePage {
 
   @ViewChild(Slides) slides:Slides;
 
+  //声明
+  searchQuery:string='';
+  items:string[];
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  //轮播
   goToSlide(){
     this.slides.slideTo(2,500);
   }
@@ -21,10 +26,36 @@ export class HomePage {
     let currentIndex = this.slides.getActiveIndex();
     console.log('index is',currentIndex);
   }
-
   ngAfterViewInit() {
     this.slides.freeMode = true;
   }
+
+  // 搜索
+  initialzeItems(){
+    this.items=[
+      '张三',
+      '李四',
+      '大王',
+      '小王',
+      '你好',
+    ];
+  }
+
+  getItems(ev:any){
+    //调取数组数据
+    this.initialzeItems();
+    //获取input框的value
+    let val = ev.target.value;
+
+    if(val && val.trim() != ''){
+      this.items = this.items.filter((item)=>{
+        return (item.toLowerCase().indexOf(val.toLowerCase())>-1);
+      })
+    }
+
+
+  }
+
 
   //初始化slides
   initSlides(){
