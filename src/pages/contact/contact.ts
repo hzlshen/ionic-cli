bffaf61d0fcc7d395f49f11675f93767ce774bf0
hzlshen <m15277968105@163.com>
2018-06-25 00:32:18 +0800

    sheet icon

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController,ActionSheetController } from 'ionic-angular';
+import { NavController,ActionSheetController,Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
@@ -7,7 +7,7 @@ import { NavController,ActionSheetController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController,public actionSheetCtrl:ActionSheetController) {
+  constructor(public platform: Platform,public navCtrl: NavController,public actionSheetCtrl:ActionSheetController) {
 
   }
   openMenu(){
@@ -16,10 +16,11 @@ export class ContactPage {
         title:"选择框",
         buttons:[
           {
-            text:'登录',
-            role:'登录',
+            text:'分享',
+            role:'destructive',
+            icon: !this.platform.is('ios') ? 'share' : null,
             handler:()=>{
-              console.log("登录")
+              console.log("分享")
             }
           },
           {
@@ -30,8 +31,8 @@ export class ContactPage {
             }
           },
           {
-            text:'退出',
-            role:'退出',
+            text:'Cancel',
+            role:'cancel',
             handler:()=>{
               console.log("退出")
             }
