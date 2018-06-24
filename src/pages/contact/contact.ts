6adabe34d5c9e2202eee354ab78b9b2df4538026
hzlshen <m15277968105@163.com>
2018-06-25 00:27:18 +0800

    ionic action sheet

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +7,37 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public actionSheetCtrl:ActionSheetController) {
 
   }
-
+  openMenu(){
+     //声明数组
+      const actionSheet = this.actionSheetCtrl.create({
+        title:"选择框",
+        buttons:[
+          {
+            text:'登录',
+            role:'登录',
+            handler:()=>{
+              console.log("登录")
+            }
+          },
+          {
+            text:'注册',
+            role:'注册',
+            handler:()=>{
+              console.log("注册")
+            }
+          },
+          {
+            text:'退出',
+            role:'退出',
+            handler:()=>{
+              console.log("退出")
+            }
+          }
+        ]
+      })
+      actionSheet.present();
+    }
 }
