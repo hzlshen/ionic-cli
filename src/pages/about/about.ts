3f7f1fe753e78c8b87c6feb4b5e830a9044b5936
hzlshen <m15277968105@163.com>
2018-06-28 21:59:57 +0800

    menu'

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, App, MenuController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +7,8 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,app:App,menu:MenuController) {
+    menu.enable(true)
   }
 }
 
