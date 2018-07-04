1f6d7d511beb00f3ad41fe9606da3579de77d873
hzlshen <m15277968105@163.com>
2018-07-04 23:38:35 +0800

    import

--- src/pages/segment/segment.ts
+++ src/pages/segment/segment.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { IonicPage, Platform } from 'ionic-angular';
 
 /**
  * Generated class for the SegmentPage page.
