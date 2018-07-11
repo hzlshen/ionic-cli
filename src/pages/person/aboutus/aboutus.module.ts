f3b0dc3680576c0ba1b49f609596740ae188a6b1
hzlshen <m15277968105@163.com>
2018-07-11 23:58:56 +0800

    关于我们

---
+++ src/pages/person/aboutus/aboutus.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AboutusPage } from './aboutus';
+
+@NgModule({
+  declarations: [
+    AboutusPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AboutusPage),
+  ],
+})
+export class AboutusPageModule {}
